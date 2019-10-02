4832bb215ea49574e482683a69bbc516c861f1be
Thiago Moura <thiago@hjf.local>
2019-10-02 17:47:56 -0300

    bug fix model e page

--- vendor/fasor/php-classes/src/Model.php
+++ vendor/fasor/php-classes/src/Model.php
@@ -22,7 +22,7 @@ class Model {
 
   public function setData($data = array()) {
     foreach ($data as $key => $value) {
-      $this -> {"set".key}($value);
+      $this -> {"set".$key}($value);
     }
   }
 
--- vendor/fasor/php-classes/src/Page.php
+++ vendor/fasor/php-classes/src/Page.php
@@ -37,7 +37,7 @@ class Page {
     }
   }
 
-  private function setTpl($name, $data = array(), $returnHTML = false) {
+  public function setTpl($name, $data = array(), $returnHTML = false) {
     $this -> setData($data);
     return $this -> tpl -> draw($name, $returnHTML);
   }
